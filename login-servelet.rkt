a2957a7af74954127eb59b692086b99107f5b19d
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2025-01-12 14:19:21 +0000

    Create login-servelet.rkt

---
+++ login-servelet.rkt
@@ -0,0 +1,7 @@
+#lang web-server/insta
+
+;; demo servelet
+;; if user is not logged in show a username/password form
+;; if user is logged in show username and logout link
+;; when servelet runs check for 'logged in' cookie
+;; Other  
